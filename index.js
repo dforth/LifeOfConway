883a0fcb8ac2025b0b419a25ec785333496e9b07
Donald Forth <donaldforth@Donalds-MacBook-Air.local>
2017-10-11 18:15:41 -0700

    Some functionality working now. Need to improve performance and add some controls

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 import * as util from './src/util.js';
 
 
-function createDataArray(width, height) {
-
-	let data = new Array(width);
-	for (var x = 0; x < width; x++) {
-		data[x] = new Array(height);
-	}
-	
-	return data;
-}
-
-
-
 class LifeOfConway {
 	
 	constructor(width, height, containerId, wrapFlag) {
 	
 		this.width = width;
 		this.height = height;	
-		this.containerId = containerId;
 		this.running = false;
 		this.cycle = 0;
 		this.wrapFlag = wrapFlag
 		
-		this.data = createDataArray(this.width, this.height);
+		// add a canvas to the container
+		let container = document.getElementById(containerId);
+		
+		var canvas = document.createElement('canvas');
+		
+		let canvasId = containerId + '-canvas';
+		this.canvasId = canvasId;
+        canvas.id = canvasId;
+        canvas.width = this.width;
+        canvas.height = this.height;
+        //canvas.style.zIndex   = 8;
+        //canvas.style.position = "absolute";
+        //canvas.style.border   = "1px solid";
+        //canvas.style.background = "red";
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        container.appendChild(canvas);
+		
+		
+		// Create the data array
+		this.data = util.createDataArray(this.width, this.height);
 		
 		this.randomize();
 		this.draw();
+		//this.step();
+		
+		canvas.addEventListener('click', this.toggle.bind(this), false);
 	}
+	
+	
 	
 
 	countNeighbors(x, y) {
@@ -47,7 +59,7 @@ class LifeOfConway {
 				
 				if (i >= 0 && i < this.width && j >= 0 && j < this.height) {
 					
-					if (this.data[i][j] > 0) {
+					if (this.data[i][j] > 0 && !(x == i && y == j)) {
 						
 						count ++;
 					}
@@ -58,28 +70,50 @@ class LifeOfConway {
 		return count;
 	}	
 	
+	toggle() {
+		
+		if (this.running) {
+			
+			this.stop();
+			
+		} else {
+						
+			this.run();
+		}
+	}
+	
 	run() {
+		
+		this.running = true;
+		this.cycle = 0;
+		
+		this.interval = setInterval(function() { 
+			
+			this.step();
+			this.cycle = this.cycle + 1;
+			
+		}.bind(this), 100);
 		
 	}
 	
 	step() {
 
-		let newData = createDataArray(this.width, this.height);
-				
+		let newData = util.createDataArray(this.width, this.height);
+
 		for (var x=0; x<this.width; x++) {
 			
 			for (var y=0; y<this.height; y++) {
 				
-				let count = countNeightbors(x,y);
+				let count = this.countNeighbors(x,y);
 				let oldValue = this.data[x][y];
-				
+								
 				newData[x][y] = 0;
 				
 				if (oldValue == 0) {
 					
 					if (count == 3) {
 						
-						newData[x][y] = 1;						
+						newData[x][y] = 1;
 					}
 					
 				} else {
@@ -88,16 +122,25 @@ class LifeOfConway {
 						
 						newData[x][y] = oldValue + 1;
 					}					
-				} 													
+				} 				
+				
+				console.groupEnd();									
 			}			
 		}
+
+		this.data = newData;	
 		
-		this.data = newData;
-		
+		this.draw();	
 	}
 	
 	stop() {
 		
+		this.running = false;
+		
+		if (this.interval) {
+			
+			clearInterval(this.interval);
+		}
 	}
 	
 	clear() {
@@ -126,7 +169,26 @@ class LifeOfConway {
 	
 	draw() {
 	
-		console.log("data = ", this.data);	
+		let canvas = document.getElementById(this.canvasId);
+		let ctx = canvas.getContext("2d");
+		ctx.fillStyle = "#000000";
+		
+		for(var x=0; x<this.width; x++) {
+			
+			for(var y=0; y<this.height; y++) {
+				
+				if (this.data[x][y] == 0) {
+					
+					ctx.clearRect(x, y, x+1, y+1);
+					
+				} else {
+					
+					ctx.fillRect(x, y, x+1, y+1);			
+				}
+			}			
+		}
+		
+		
 	}			
 }
 
--- src/util.js
+++ src/util.js
@@ -5,3 +5,15 @@ export function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
+
+export function createDataArray(width, height) {
+
+	let result = new Array(width);
+	
+	for (var x = 0; x < width; x++) {
+		
+		result[x] = new Array(height).fill(0);
+	}
+		
+	return result;
+}
